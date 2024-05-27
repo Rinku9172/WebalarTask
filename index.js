6d986a9f826d9013ec7909b3ab51f4d9e80d6c84
rinkuJat <jatr26008@gmail.com>
2024-05-27 16:01:32 +0530

    Changes made

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 const connectToMongo = require('./db');
-const express = require('express')
-var cors = require('cors')
+const express = require('express');
+var cors = require('cors');
 connectToMongo();
-const app = express()
-const port = 5000
+const app = express();
+const port = 5000;
 
-app.use(cors())
-app.use(express.json())
+// Configure CORS to allow specific origin
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow requests from this origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow these methods
+  credentials: true // Allow credentials such as cookies, authorization headers or TLS client certificates
+}));
 
-router.get('/', async (req, res) => {
-res.send("Backend is working");
-})
+app.use(express.json());
 
-//Available Routes
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/task', require('./routes/tasks'))
+app.get('/', async (req, res) => {
+  res.send("Backend is working");
+});
 
+// Available Routes
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/task', require('./routes/tasks'));
 
 app.listen(port, () => {
-  console.log(`PreetMakeup backend listening at http://localhost:${port}`)
-})
+  console.log(`PreetMakeup backend listening at http://localhost:${port}`);
+});
